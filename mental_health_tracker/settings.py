7eea52ed7c1ec870fa767d443337ee0a66f763d7
alicfamahdi <alicfamahdi@gmail.com>
2024-08-28 15:41:22 +0700

    ahay

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-92tms=4y28xs9pe_*ra(547kmm!lj+6$m3%!*1vfkz6!ucr4nt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://pbp.cs.ui.ac.id/aliefa.alsyafiandra/mentalhealthtracker"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "aliefa-alsyafiandra-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 # Application definition
 
