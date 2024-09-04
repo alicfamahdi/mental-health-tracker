50fddd44f1cdcc86f331e819aa995d89feb59358
alicfamahdi <alicfamahdi@gmail.com>
2024-09-04 14:32:33 +0700

    tut 1

---
+++ mental_health_tracker/templates/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'npm' : '2306221056',
+        'name': 'Aliefa Alsyafiandra Setiawati Mahdi',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
